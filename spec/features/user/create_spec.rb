d571f059207569f6180b362383bdfa4220222c02
Sage Lee <sagemcklee@gmail.com>
2020-05-27 14:59:39 -0600

    Finish user story 12

--- spec/features/user/create_spec.rb
+++ spec/features/user/create_spec.rb
@@ -28,7 +28,6 @@ RSpec.describe "User registration form" do
     expect(page).to have_content("Welcome, George! You are now registered, and logged in.")
   end
 
-
   it "will not create user with missing info" do
 
     visit "/register"
@@ -57,6 +56,39 @@ RSpec.describe "User registration form" do
 
     expect(current_path).to eq("/profile")
     expect(page).to have_content("Welcome, George! You are now registered, and logged in.")
-    save_and_open_page
+  end
+
+  it "will not create user with incorrect info" do
+
+    User.create(name: "Cardi", address: '123 Dog Rd.', city: 'Denver', state: 'CO', zip: 80204, email: 'coronavirusitsgettingreal@gmail.com', password: "iloveoffset")
+
+
+    visit "/register"
+
+    email =  "georgef@gmail.com"
+    password = "grillingiscool"
+
+    fill_in :name, with: "George"
+    fill_in :address, with: "3 Main st."
+    fill_in :state, with: "MA"
+    fill_in :city, with: "Boston"
+    fill_in :zip, with: "12345"
+    fill_in :email, with: 'coronavirusitsgettingreal@gmail.com'
+    fill_in :password, with: password
+    click_on "Create User"
+
+    expect(page).to have_content("Email has already been taken")
+
+    fill_in :name, with: "George"
+    fill_in :address, with: "3 Main st."
+    fill_in :state, with: "MA"
+    fill_in :city, with: "Boston"
+    fill_in :zip, with: "12345"
+    fill_in :email, with: email
+    fill_in :password, with: password
+    click_on "Create User"
+
+    expect(current_path).to eq("/profile")
+    expect(page).to have_content("Welcome, George! You are now registered, and logged in.")
   end
 end
