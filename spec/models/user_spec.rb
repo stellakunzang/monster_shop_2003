e7337bef35ec96103a587aeb6b88c9ecb04f4ef0
Sage Lee <sagemcklee@gmail.com>
2020-05-26 21:26:41 -0600

    Pass user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Item, type: :model do
+describe User, type: :model do
   describe "validations" do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
