7f7bedb2138ea000ba4bff0a90d177fb74716a09
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-27 20:17:48 -0600

    Add test for 5 least popular items

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -86,15 +86,45 @@ RSpec.describe "Items Index Page" do
       visit "/items"
 
       expect(page).to have_content("Top 5 Most Popular Items:")
-      expect(page).to have_content("1) #{@dog_bone.name} (Total Purchased = 8)")
-      expect(page).to have_content("2) #{@pull_toy.name} (Total Purchased = 5)")
-      expect(page).to have_content("3) #{@tire.name} (Total Purchased = 4)")
-      expect(page).to have_content("4) #{toy.name} (Total Purchased = 3)")
-      expect(page).to have_content("5) #{bone.name} (Total Purchased = 2)")
-
+      expect(@dog_bone.name).to appear_before(@pull_toy.name)
+      expect(page).to have_content("Total Purchased: 8")
+      expect(@pull_toy.name).to appear_before(@tire.name)
+      expect(page).to have_content("Total Purchased: 5")
+      expect(@tire.name).to appear_before(toy.name)
+      expect(page).to have_content("Total Purchased: 4")
+      expect(toy.name).to appear_before(bone.name)
+      expect(page).to have_content("Total Purchased: 3")
+      expect(page).to have_content("Total Purchased: 2")
     end
 
-    it "All users can see 5 least popular, including quantity purchased" do
-    end 
+    xit "All users can see 5 least popular, including quantity purchased" do
+      bone = @brian.items.create(name: "Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
+
+      toy = @brian.items.create(name: "Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+
+      order = Order.create!(name: "name", address: "address", city: "city", state: "state", zip: 23455)
+      order2 = Order.create!(name: "name", address: "address", city: "city", state: "state", zip: 23455)
+
+      ItemOrder.create!(order_id: order.id, price: 1.0, item_id: @dog_bone.id, quantity: 5)
+      ItemOrder.create!(order_id: order.id, price: 1.0, item_id: @pull_toy.id, quantity: 1)
+      ItemOrder.create!(order_id: order.id, price: 1.0, item_id: @tire.id, quantity: 4)
+      ItemOrder.create!(order_id: order.id, price: 1.0, item_id: toy.id, quantity: 3)
+      ItemOrder.create!(order_id: order.id, price: 1.0, item_id: bone.id, quantity: 2)
+      ItemOrder.create!(order_id: order2.id, price: 1.0, item_id: @dog_bone.id, quantity: 3)
+      ItemOrder.create!(order_id: order2.id, price: 1.0, item_id: @pull_toy.id, quantity: 4)
+
+      visit "/items"
+
+      expect(page).to have_content("Top 5 Most Popular Items:")
+      expect(@dog_bone.name).to appear_before(@pull_toy.name)
+      expect(page).to have_content("Total Purchased: 8")
+      expect(@pull_toy.name).to appear_before(@tire.name)
+      expect(page).to have_content("Total Purchased: 5")
+      expect(@tire.name).to appear_before(toy.name)
+      expect(page).to have_content("Total Purchased: 4")
+      expect(toy.name).to appear_before(bone.name)
+      expect(page).to have_content("Total Purchased: 3")
+      expect(page).to have_content("Total Purchased: 2")
+    end
   end
 end
