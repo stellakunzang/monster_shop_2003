81ac1436dccd2148ff3f1e59e44ee243faaac41a
Danny Ramos <danramos21@gmail.com>
2020-05-27 20:03:09 -0600

    Part 2 of 13 complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:name,:address,:city,:state,:zip, :email,:password)
+    params.permit(:name, :address, :city, :state, :zip, :email, :password)
 
   end
 end
