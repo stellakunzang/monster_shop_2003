3cd96706bce15c2675241896611c36fcaeccff40
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-29 17:15:02 -0600

    Remove extra variable from controller so only passing one thing to view

--- app/controllers/merch_controller.rb
+++ app/controllers/merch_controller.rb
@@ -1,10 +1,10 @@
 class MerchController <ApplicationController
+
   def show
     if current_user == nil || current_user.role != "merchant"
       redirect_to "/error404"
     else
       @merchant = User.find_by(id: session[:user_id])&.merchant
-      @orders = @merchant.orders_with_my_items 
     end
   end
 end
