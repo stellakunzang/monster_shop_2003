059b62e770d28fc7e5770ca21cdb42e3dc0ebf89
Sage Lee <sagemcklee@gmail.com>
2020-05-28 22:09:03 -0600

    Add My Orders button to profile show page

--- spec/features/user/profile_spec.rb
+++ spec/features/user/profile_spec.rb
@@ -106,7 +106,12 @@ RSpec.describe "User profile page" do
   end
 
   it "displays link for orders if orders have been placed" do
-    order = @regular_user.order.create!(name: "name", address: "address", city: "city", state: "state", zip: 23455)
+    bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+    tire = bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+    pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+    dog_bone = dog_shop.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
+    order = Order.create!(name: "name", address: "address", city: "city", state: "state", zip: 23455)
     ItemOrder.create!(order_id: order.id, price: 1.0, item_id: dog_bone.id, quantity: 5)
     ItemOrder.create!(order_id: order.id, price: 1.0, item_id: pull_toy.id, quantity: 1)
     ItemOrder.create!(order_id: order.id, price: 1.0, item_id: tire.id, quantity: 4)
@@ -122,11 +127,3 @@ RSpec.describe "User profile page" do
     expect(current_path).to eq('/profile/orders')
   end
 end
-
-# User Story 27, User Profile displays Orders link
-#
-# As a registered user
-# When I visit my Profile page
-# And I have orders placed in the system
-# Then I see a link on my profile page called "My Orders"
-# When I click this link my URI path is "/profile/orders"
