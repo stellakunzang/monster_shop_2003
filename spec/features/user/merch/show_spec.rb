fa9a515416b806ccb559aed55c83d83350a2c638
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-28 19:26:34 -0600

    Remove merchant_id from user in setup

--- spec/features/user/merch/show_spec.rb
+++ spec/features/user/merch/show_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe "Merchant employee dashboard show page" do
 
   before(:each) do
-    @dog_shop = Merchant.create(name: "Meg's Dog Shop", address: '123 Dog Rd.', city: 'Hershey', state: 'PA', zip: 80203)
-    @maude = dog_shop.users.create(name: "Maude Sloggett", address: "17 Sun Rise St", city: "El Paso", state: "Illinois", zip: "56726", email: "M.Slogget@yahoo.com", password: "Forever27", role: 1, merchant_id: dog_shop.id)
+    @dog_shop = Merchant.create!(name: "Meg's Dog Shop", address: '123 Dog Rd.', city: 'Hershey', state: 'PA', zip: 80203)
+    @maude = @dog_shop.users.create!(name: "Maude Sloggett", address: "17 Sun Rise St", city: "El Paso", state: "Illinois", zip: "56726", email: "M.Slogget@yahoo.com", password: "Forever27", role: 1)
 
     visit '/login'
     fill_in :email, with: @maude.email
@@ -21,6 +21,6 @@ RSpec.describe "Merchant employee dashboard show page" do
     expect(page).to have_content(@dog_shop.city)
     expect(page).to have_content(@dog_shop.state)
     expect(page).to have_content(@dog_shop.zip)
-    
+
   end
 end
