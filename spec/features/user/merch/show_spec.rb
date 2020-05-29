27302b6f7cbaff37a0c191e0d6fbe64186e2e20c
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-28 20:00:53 -0600

    Create test for merchant dashboard pending orders

--- spec/features/user/merch/show_spec.rb
+++ spec/features/user/merch/show_spec.rb
@@ -23,4 +23,27 @@ RSpec.describe "Merchant employee dashboard show page" do
     expect(page).to have_content(@dog_shop.zip)
 
   end
+
+  it "displays orders with pending status that my shop sells" do
+    bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+    dog_bone = dog_shop.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", inventory: 21)
+    tire = bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+
+    order = Order.create!(name: "name", address: "address", city: "city", state: "state", zip: 23455)
+
+    ItemOrder.create!(order_id: order.id, price: 1.0, item_id: dog_bone.id, quantity: 5)
+    ItemOrder.create!(order_id: order.id, price: 1.0, item_id: pull_toy.id, quantity: 1)
+    ItemOrder.create!(order_id: order.id, price: 1.0, item_id: tire.id, quantity: 4)
+
+    visit "/merchants"
+
+    expect(page).to have_content(order.id)
+    click_on order.id
+    expect(current_path).to equal("/merchants/orders/#{order.id}")
+
+    expect(page).to have_content(order.created_at)
+    expect(page).to have_content("6 Items Total")
+    expect(page).to have_content("$6.00")
+  end
 end
