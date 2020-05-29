6d4be3b9c7c213a245b1cfeb347e040482bdb28c
Michael Gallup <Gallup93@icloud.com>
2020-05-28 21:34:49 -0600

    Refactor admin index_spec for story 40, Add enable link for merchants

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "Admin index page (/admin/merchants)" do
-  it "displays all merchants with functioning buttons to disable (if active)" do
+RSpec.describe "Admin from their index page (/admin/merchants)" do
+  it "displays all merchants with functioning buttons to disable and enable merchants" do
     merch1 = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
     merch2 = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
     merch2.update_attribute(:active?, false)
@@ -13,13 +13,19 @@ RSpec.describe "Admin index page (/admin/merchants)" do
       click_link "disable"
       expect(current_path).to eq("/admin/merchants")
       expect(page).to_not have_link("disable")
+      expect(page).to have_link("enable")
     end
 
     expect(page).to have_content("disabled #{merch1.name}")
 
     within ".merchant-#{merch2.id}" do
-      expect(page).to_not have_link("disable")
+      click_link "enable"
+      expect(current_path).to eq("/admin/merchants")
+      expect(page).to have_link("disable")
+      expect(page).to_not have_link("enable")
     end
+
+    expect(page).to have_content("enabled #{merch2.name}")
 
     within ".merchant-#{merch3.id}" do
       click_link "disable"
