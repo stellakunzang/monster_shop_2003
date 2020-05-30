c3f379d8c45da5aaa79060d7528e9950a9eba837
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-30 10:41:42 -0600

    Simplify method to only grab order ids

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -27,33 +27,38 @@ class Merchant <ApplicationRecord
     item_orders.distinct.joins(:order).pluck(:city)
   end
 
-  def orders_with_my_items
-    orders_with_data = Hash.new { |hash, key| hash[key] = {date: 0, quantity_sum: 0, price_sum: 0} }
-    find_quantities.each do |id, quantity|
-      orders_with_data[id][:quantity_sum] = quantity
-      orders_with_data[id][:date] = Order.find_by(id: id).created_at.strftime('%m/%d/%Y')
-    end
-    find_prices.each do |id, price|
-      orders_with_data[id][:price_sum] = price
-    end
-    orders_with_data
+  def merchant_orders
+    items.joins(:orders).where('status =?', 0).distinct.pluck('order_id')
   end
-  # also still haven't added pending status!
-  # still not multiplying by the quantity
-
-  def find_prices
-    items.joins(:item_orders)
-         .select('item_orders.order_id, item_orders.price, items.merchant_id')
-         .where('items.merchant_id =?', self.id)
-         .group('item_orders.order_id')
-         .sum('item_orders.price')
-  end
-
-  def find_quantities
-    items.joins(:item_orders)
-         .select('item_orders.order_id, item_orders.quantity, item_orders.created_at, items.merchant_id')
-         .where('items.merchant_id =?', self.id)
-         .group('item_orders.order_id')
-         .sum('item_orders.quantity')
-  end
+    #        .select('item_orders.order_id, item_orders.price, items.merchant_id')
+    #        .where('items.merchant_id =?', self.id)
+    #        .group('item_orders.order_id')
+    #        .sum('item_orders.price')
+    # orders_with_data = Hash.new { |hash, key| hash[key] = {date: 0, quantity_sum: 0, price_sum: 0} }
+  #   find_quantities.each do |id, quantity|
+  #     orders_with_data[id][:quantity_sum] = quantity
+  #     orders_with_data[id][:date] = Order.find_by(id: id).created_at.strftime('%m/%d/%Y')
+  #   end
+  #   find_prices.each do |id, price|
+  #     orders_with_data[id][:price_sum] = price
+  #   end
+  #   orders_with_data
+  # end
+  # # also still haven't added pending status!
+  # # still not multiplying by the quantity
+  #
+  # def find_prices
+  #   items.joins(:item_orders)
+  #        .select('item_orders.order_id, item_orders.price, items.merchant_id')
+  #        .where('items.merchant_id =?', self.id)
+  #        .group('item_orders.order_id')
+  #        .sum('item_orders.price')
+  # end
+  #
+  # def find_quantities
+  #   items.joins(:item_orders)
+  #        .select('item_orders.order_id, item_orders.quantity, item_orders.created_at, items.merchant_id')
+  #        .where('items.merchant_id =?', self.id)
+  #        .group('item_orders.order_id')
+  #        .sum('item_orders.quantity')
 end
