e8adfb2bf1060d591ca43398c1afe31611df044b
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-30 23:08:18 -0600

    Add second variable to show method in order to pass merchant to view

--- app/controllers/merchant/orders_controller.rb
+++ app/controllers/merchant/orders_controller.rb
@@ -2,6 +2,7 @@ class Merchant::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:order_id])
+    @merchant = Merchant.find(current_user[:merchant_id])
   end
 
 end
