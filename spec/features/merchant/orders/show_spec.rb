fdeb31de873defd6fc347726df096bda5e70c695
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-31 19:19:20 -0600

    Write tests for merchant order fulfillment

--- spec/features/merchant/orders/show_spec.rb
+++ spec/features/merchant/orders/show_spec.rb
@@ -5,15 +5,17 @@ RSpec.describe "Merchant employee view of order show page" do
   before(:each) do
     @dog_shop = Merchant.create!(name: "Meg's Dog Shop", address: '123 Dog Rd.', city: 'Hershey', state: 'PA', zip: 80203)
     @maude = @dog_shop.users.create!(name: "Maude Sloggett", address: "17 Sun Rise St", city: "El Paso", state: "Illinois", zip: "56726", email: "M.Slogget@yahoo.com", password: "Forever27", role: 1)
-    @bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
     @pull_toy = Item.create!(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32, merchant_id: @dog_shop.id)
-    @dog_bone = Item.create!(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", inventory: 21, merchant_id: @dog_shop.id)
+    @dog_bone = Item.create!(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", inventory: 5, merchant_id: @dog_shop.id)
+
+    @bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
     @tire = @bike_shop.items.create!(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+
     visit '/login'
     fill_in :email, with: @maude.email
     fill_in :password, with: @maude.password
     click_on "Login!"
-    
+
     @order = Order.create!(name: "name", address: "address", city: "city", state: "state", zip: 23455, user_id: @maude.id)
 
     @item_order_1 = ItemOrder.create!(order_id: @order.id, price: 1.0, item_id: @dog_bone.id, quantity: 5)
@@ -57,5 +59,32 @@ RSpec.describe "Merchant employee view of order show page" do
     expect(page).to have_css("img[src*='#{@pull_toy.image}']")
     expect(page).to have_content("#{@item_order_1.quantity}")
   end
+
+  it "if order is unfulfilled and the desired quantity is equal to or less than my inventory, I see fulfill button" do
+    visit "/merchant/orders/#{@order.id}"
+    within dog_bones do
+      click_on "fulfill item"
+    end
+
+    expect(current_path).to eq("/merchant/orders/#{@order.id}")
+    expect(@dog_bone.inventory).to eq(0)
+    expect(page).to have_content("#{@dog_bone.name} from order # #{@order.id} has been fulfilled.")
+
+    within dog_bones do
+      expect(page).to_not have_content("fulfill item")
+      expect(page).to have_content("This item has already been fulfilled")
+    end
+  end
+
+  it "cannot fulfill if inventory is less than desired purchase amount" do
+    new_order = Order.create!(name: "name", address: "address", city: "city", state: "state", zip: 23455, user_id: @maude.id)
+    ItemOrder.create!(order_id: new_order.id, price: 1.0, item_id: @dog_bone.id, quantity: 6)
+
+    visit "/merchant/orders/#{new_order.id}"
+
+    expect(page).to have_content("There is not enough inventory to fulfill this item")
+  end
+
+
 
 end
