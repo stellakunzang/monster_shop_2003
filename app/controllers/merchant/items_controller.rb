3f7f5ad2b102078edbf4902bf585eea93900dfef
Michael Gallup <Gallup93@icloud.com>
2020-06-01 17:18:16 -0600

    Add update action for merchant items

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -3,4 +3,20 @@ class Merchant::ItemsController < ApplicationController
   def index
     @merchant = Merchant.find(current_user[:merchant_id])
   end
+
+  def update
+    if current_user == nil || current_user.role != "merchant"
+      redirect_to "/error404"
+    else
+      item = Item.find(params[:id])
+      if item.active? == true
+        item.update(active?: false)
+        flash[:notice] = "#{item.name} is now inactive!"
+      else
+        item.update(active?: true)
+        flash[:notice] = "#{item.name} is now active!"
+      end
+      redirect_to "/merchant/items"
+    end
+  end
 end
