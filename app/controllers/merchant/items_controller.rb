50240a2581cdd8f5f6e6455fdad36e2e89203dd6
Danny Ramos <danramos21@gmail.com>
2020-06-02 16:50:07 -0600

    Random syntax error

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -24,7 +24,8 @@ class Merchant::ItemsController < ApplicationController
     else
       flash[:error] = item.errors.full_messages.to_sentence
       render :new
-
+    end
+  end
   def status
     if current_user == nil || current_user.role != "merchant"
       redirect_to "/error404"
@@ -64,8 +65,6 @@ class Merchant::ItemsController < ApplicationController
   private
 
   def item_params
-    
     params.permit(:name,:description,:price,:inventory,:image,:active?)
-
   end
 end
