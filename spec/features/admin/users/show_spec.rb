19ac49fa6af0907c79479caff1a74d2dd95494a0
stellakunzang <stella.k.bonnie@gmail.com>
2020-06-02 14:38:25 -0600

    Unskip test

--- spec/features/admin/users/show_spec.rb
+++ spec/features/admin/users/show_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe "Admin view of user profile page" do
     expect(page).to_not have_link("Edit My Profile")
   end
 
-  xit "displays link for orders if orders have been placed" do
+  it "displays link for orders if orders have been placed" do
     bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
     dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
     tire = bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
@@ -36,4 +36,5 @@ RSpec.describe "Admin view of user profile page" do
     click_link("My Orders")
     expect(current_path).to eq('/profile/orders')
   end
+
 end
