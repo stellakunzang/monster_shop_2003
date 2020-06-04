b75baf69a20d6e5f3082cbe741213f889848eaa0
stellakunzang <stella.k.bonnie@gmail.com>
2020-06-03 23:59:56 -0600

    Deleted comment

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -8,14 +8,6 @@ class AdminsController < ApplicationController
     end
   end
   
-  # def show
-  #   if current_user == nil || current_user.role != "admin"
-  #     redirect_to "/error404"
-  #   else
-  #     @user = User.find(session[:user_id])
-  #   end
-  # end
-
   def create
     new_user = User.new(user_params)
     if new_user.save
