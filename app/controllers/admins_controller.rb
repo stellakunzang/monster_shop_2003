7585ab87488b3bfeec6e546db0fb29ac48a5bf3e
Michael Gallup <Gallup93@icloud.com>
2020-06-04 10:39:39 -0600

    Remove unused code

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -7,16 +7,4 @@ class AdminsController < ApplicationController
       @orders = Order.all
     end
   end
-
-  def create
-    new_user = User.new(user_params)
-    if new_user.save
-      session[:user_id] = new_user.id
-      redirect_to "/profile"
-    else
-      flash.now[:notice] = new_user.errors.full_messages
-      render :new
-    end
-  end
-
 end
