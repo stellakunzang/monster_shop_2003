8a78946676155dd798fc88df3b9dd89762c4db90
stellakunzang <stella.k.bonnie@gmail.com>
2020-06-20 20:04:33 -0600

    Update reviews routes

--- config/routes.rb
+++ config/routes.rb
@@ -41,12 +41,11 @@ Rails.application.routes.draw do
   get "/merchants/:merchant_id/items/new", to: "merchants_items#new"
   post "/merchants/:merchant_id/items", to: "merchants_items#create"
 
-  get "/items/:item_id/reviews/new", to: "reviews#new"
-  post "/items/:item_id/reviews", to: "reviews#create"
-
-  get "/reviews/:id/edit", to: "reviews#edit"
-  patch "/reviews/:id", to: "reviews#update"
-  delete "/reviews/:id", to: "reviews#destroy"
+  resources :items do
+    resources :reviews, only: [:new, :create]
+  end
+  
+  resources :reviews, only: [:edit, :update, :destroy]
 
   get "/cart", to: "cart#show"
   delete "/cart", to: "cart#destroy"
